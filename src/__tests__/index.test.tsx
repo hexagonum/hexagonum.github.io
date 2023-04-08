80a04d99dbcd6cd06a02fd66df90df9ca3a461f6
hieudoanm <hieumdoan@gmail.com>
2023-04-08 21:06:47 +0700

    Update Snapshot

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,6 +1,10 @@
 import { render } from '@testing-library/react';
 import { HomePage } from '../pages/index';
 
+jest.mock('next/router', () => ({
+  useRouter: jest.fn().mockReturnValue({ query: { visible: 'true' } }),
+}));
+
 describe('HomePage', () => {
   it('to match snapshot', () => {
     const { container } = render(<HomePage />);
