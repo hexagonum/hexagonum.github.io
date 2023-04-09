41b34949616abf238e95fe3d4b7d09dacf23ebce
hieudoanm <hieumdoan@gmail.com>
2023-04-09 09:59:48 +0700

    Add PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document: React.FC = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>{' '}
+        <meta name="theme-color" content="#000000" />
+      </Head>
       <body>
         <Main />
         <NextScript />
