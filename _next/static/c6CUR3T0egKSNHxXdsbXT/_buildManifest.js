ae1021df25b60bf42ec2cfbc40a09cf72f612186
hieudoanm <hieumdoan@gmail.com>
2023-04-13 10:15:24 +0000

    Deploying to gh-pages from @ hexagonum/hexagonum.github.io@834edd8e46e46f5a771e139e8def5548cfdd3065 🚀

---
+++ _next/static/c6CUR3T0egKSNHxXdsbXT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a4af18ca04b2ffb3.js"],"/404":["static/chunks/pages/404-f4427799678d7ee6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
